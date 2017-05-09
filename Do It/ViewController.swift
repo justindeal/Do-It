e2140db92193ad5ad3cf6276e9ce3c8d515c9678
Justin Deal <justindeal86@gmail.com>
2017-05-09 19:58:24 -0400

    Initial Commit

---
+++ Do It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Do It
+//
+//  Created by Justin Deal on 5/9/17.
+//  Copyright © 2017 J Dub Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
