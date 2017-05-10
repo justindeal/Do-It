8e41b24f8dde4f2c706d7a4323ab587d6b09f4ed
Justin Deal <justindeal86@gmail.com>
2017-05-09 21:33:56 -0400

    Added task class swift file and created if then for important tasks

---
+++ Do It/task.swift
@@ -0,0 +1,14 @@
+//
+//  task.swift
+//  Do It
+//
+//  Created by Justin Deal on 5/9/17.
+//  Copyright © 2017 J Dub Apps. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
