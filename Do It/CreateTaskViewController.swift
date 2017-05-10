ea0ae177d5e947615204daa5a9199b5ccfcd61e8
Justin Deal <justindeal86@gmail.com>
2017-05-09 23:06:38 -0400

    Added visuals and connected them to CreateTaskViewController

---
+++ Do It/CreateTaskViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateTaskViewController.swift
+//  Do It
+//
+//  Created by Justin Deal on 5/9/17.
+//  Copyright © 2017 J Dub Apps. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+   
+
+}
